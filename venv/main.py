07bb05e4b862556089d1fab8f11075c3205800e2
SnUmnui <vs.anps@gmail.com>
2022-02-13 22:29:27 +0200

    add new branch

--- venv/main.py
+++ venv/main.py
@@ -1,13 +1,13 @@
 from aiogram.utils import executor
 from create_bot import dp, bot
 from Handlers import client, admin, other
-async def on_startup():
-    print("Bot started")
+#async def on_startup():
+   # print("Bot started")
 
 
 client.register_handlers_client(dp)
 other.register_handlers_other(dp)
 
 
-executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
-#test 1
+executor.start_polling(dp, skip_updates=True)#on_startup=on_startup
+#test 11
