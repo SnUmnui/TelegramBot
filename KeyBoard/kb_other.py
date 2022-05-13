6098b6f88fb3030ab0e2ca34cca117d1505e5668
SnUmnui <vs.anps@gmail.com>
2022-05-13 13:16:09 +0300

    add descripshion

--- KeyBoard/kb_other.py
+++ KeyBoard/kb_other.py
@@ -1,9 +1,9 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-choice_dep = InlineKeyboardMarkup(row_width=2)
-dep_1 = InlineKeyboardButton(text="dep_1", callback_data="dep_1")
-dep_2 = InlineKeyboardButton(text="dep_2", callback_data="dep_2")
+choice_dep = InlineKeyboardMarkup(row_width=3)
+dep_1 = InlineKeyboardButton(text="Головний корпус", callback_data="dep_1")
+dep_2 = InlineKeyboardButton(text="Корпус №2", callback_data="dep_2")
+dep_3 = InlineKeyboardButton(text="Корпус №3", callback_data="dep_3")
 
-choice_dep.insert(dep_1)
-choice_dep.insert(dep_2)
+choice_dep.add(dep_1).add(dep_2).add(dep_3)
 
